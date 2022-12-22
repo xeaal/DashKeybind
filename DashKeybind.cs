97bdf5a804a33c8381e1edda65460e3939049b20
xeaal <tryax.use@gmail.com>
2022-12-23 01:47:52 +0500

    README.md edit

--- DashKeybind.cs
+++ DashKeybind.cs
@@ -1,14 +1,17 @@
+using System;
+using System.Reflection;
 using Terraria;
 using Terraria.GameInput;
 using Terraria.ModLoader;
+using MonoMod.RuntimeDetour.HookGen;
 
 namespace DashKeybind {
-	public class DashKeybind : ModSystem {
+	public class DashKeybind : Mod {
 		public static ModKeybind Dash {
 			get;
 			private set;
 		}
-		private void Player_DoCommonDashHandle(On.Terraria.Player.orig_DoCommonDashHandle orig, Terraria.Player self, out int dir, out bool dashing, Player.DashStartAction dashStartAction = null) {
+		private void VanillaDashPatch(On.Terraria.Player.orig_DoCommonDashHandle orig, Terraria.Player self, out int dir, out bool dashing, Player.DashStartAction dashStartAction = null) {
 			dir = 0;
 			dashing = false;
 			if (DashKeybind.Dash.JustPressed) {
@@ -53,12 +56,58 @@ namespace DashKeybind {
 			}
 		}
 		public override void Load() {
-			On.Terraria.Player.DoCommonDashHandle += Player_DoCommonDashHandle;
-			Dash = KeybindLoader.RegisterKeybind (Mod, "Dash", "Mouse3");
+			On.Terraria.Player.DoCommonDashHandle += VanillaDashPatch;
+			Dash = KeybindLoader.RegisterKeybind (this, "Dash", "Mouse3");
 		}
 		public override void Unload() {
 			Dash = null;
-			On.Terraria.Player.DoCommonDashHandle -= Player_DoCommonDashHandle;
+			On.Terraria.Player.DoCommonDashHandle -= VanillaDashPatch;
 		}
+	}
+	[JITWhenModsEnabled("CalamityMod")]
+	public sealed class CalamityDetour : ModSystem {
+		public override void Load() {
+			if (!ModLoader.HasMod("CalamityMod")) return;
+			PatchCalamity();
+		}
+		private static void PatchCalamity() {
+			HookEndpointManager.Add(typeof(CalamityMod.CalPlayer.CalamityPlayer).GetMethod("HandleHorizontalDash", BindingFlags.Public | BindingFlags.Instance), HDashPatch);
+			HookEndpointManager.Add(typeof(CalamityMod.CalPlayer.CalamityPlayer).GetMethod("HandleOmnidirectionalDash", BindingFlags.Public | BindingFlags.Instance), ODDashPatch);
+		}
+		private delegate bool orig_HandleHorizontalDash(CalamityMod.CalPlayer.CalamityPlayer self, out CalamityMod.Enums.DashDirection direction);
+		private static bool HDashPatch(orig_HandleHorizontalDash orig, CalamityMod.CalPlayer.CalamityPlayer self, out CalamityMod.Enums.DashDirection direction) {
+			direction = CalamityMod.Enums.DashDirection.Directionless;
+			bool result = false;
+			if (DashKeybind.Dash.JustPressed) {
+				switch(self.Player.direction) {
+					case -1:
+						if (self.Player.controlRight) {
+							direction = CalamityMod.Enums.DashDirection.Right;
+							result = true;
+							self.dashTimeMod=0;
+						}
+						else {
+							direction = CalamityMod.Enums.DashDirection.Left;
+							result = true;
+							self.dashTimeMod=0;
+						}
+						break;
+					case 1:
+						if (self.Player.controlLeft) {
+							direction = CalamityMod.Enums.DashDirection.Left;
+							result = true;
+							self.dashTimeMod=0;
+						}
+						else {
+							direction = CalamityMod.Enums.DashDirection.Right;
+							result = true;
+							self.dashTimeMod=0;
+						}
+						break;
+				}
+			}
+			return result;
+		}
+		private delegate bool orig_HandleOmnidirectionalDash(CalamityMod.CalPlayer.CalamityPlayer self, out CalamityMod.Enums.DashDirection direction)
 	}
 }
