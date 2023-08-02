9bc4544ddca08c7e0918dcb62a4ed56f6001203f
xeaal <tryax.use@gmail.com>
2023-08-02 07:26:31 +0500

    Update to 1.4.4

--- DashKeybind.cs
+++ DashKeybind.cs
@@ -10,10 +10,12 @@ using Mono;
 namespace DashKeybind {
 	public class DashKeybind : Mod {
 		public static ModKeybind Dash;
-		private void VanillaDashPatch(On.Terraria.Player.orig_DoCommonDashHandle orig, Terraria.Player self, out int dir, out bool dashing, Player.DashStartAction dashStartAction) {
+		private static void VanillaDashPatch(Terraria.On_Player.orig_DoCommonDashHandle orig, Terraria.Player self, out int dir, out bool dashing, Player.DashStartAction dashStartAction) {
 			dir = 0;
 			dashing = false;
-			if (DashKeybind.Dash.JustPressed) {
+			if (self.whoAmI != Main.myPlayer)
+				orig.Invoke(self, out dir, out dashing, dashStartAction);
+			else if (DashKeybind.Dash.JustPressed) {
 				switch(self.direction) {
 					case -1:
 						if (self.controlRight)
@@ -29,104 +31,35 @@ namespace DashKeybind {
 						break;
 				}
 				dashing = true;
+				if (self.dashTime > 0) {
+					self.dashTime--;
+				}
+				if (self.dashTime < 0) {
+					self.dashTime++;
+				}
+				if ((self.dashTime <= 0 && self.direction == -1) || (self.dashTime >= 0 && self.direction == 1)) {
+					self.dashTime = 15;
+					return;
+				}
+				dashing = true;
+				self.dashTime = 0;
 				self.timeSinceLastDashStarted = 0;
-				dashStartAction?.Invoke(dir);
+				if (dashStartAction != null)
+					dashStartAction?.Invoke(dir);
+			}
+			else {
+				dir = 0;
+				dashing = false;
 			}
 		}
 		public override void Load() {
-			On.Terraria.Player.DoCommonDashHandle += VanillaDashPatch;
+			if (ModLoader.HasMod("CalamityMod")) return;
+			Terraria.On_Player.DoCommonDashHandle += VanillaDashPatch;
 			Dash = KeybindLoader.RegisterKeybind (this, "Dash", "Mouse3");
 		}
 		public override void Unload() {
 			Dash = null;
-			On.Terraria.Player.DoCommonDashHandle -= VanillaDashPatch;
-		}
-	}
-	[JITWhenModsEnabled("CalamityMod")]
-	public sealed class CalamityDetour : ModSystem {
-		public override void Load() {
-			if (!ModLoader.HasMod("CalamityMod")) return;
-			PatchCalamity();
-
-		}
-		private static void PatchCalamity() {
-			HookEndpointManager.Add(typeof(CalamityMod.CalPlayer.CalamityPlayer).GetMethod("HandleHorizontalDash", BindingFlags.Public | BindingFlags.Instance), HDashPatch);
-			HookEndpointManager.Add(typeof(CalamityMod.CalPlayer.CalamityPlayer).GetMethod("HandleOmnidirectionalDash", BindingFlags.Public | BindingFlags.Instance), ODDashPatch);
-			HookEndpointManager.Modify(typeof(CalamityMod.CalPlayer.CalamityPlayer).GetMethod("ProcessTriggers", BindingFlags.Public | BindingFlags.Instance), ODDashCheckPatch);
-		}
-		private delegate bool orig_HandleHorizontalDash(CalamityMod.CalPlayer.CalamityPlayer self, out CalamityMod.Enums.DashDirection direction);
-		private static bool HDashPatch(orig_HandleHorizontalDash orig, CalamityMod.CalPlayer.CalamityPlayer self, out CalamityMod.Enums.DashDirection direction) {
-			direction = CalamityMod.Enums.DashDirection.Directionless;
-			bool result = false;
-			if (DashKeybind.Dash.JustPressed) {
-				switch(self.Player.direction) {
-					case -1:
-						if (self.Player.controlRight)
-							direction = CalamityMod.Enums.DashDirection.Right;
-						else
-							direction = CalamityMod.Enums.DashDirection.Left;
-						break;
-					case 1:
-						if (self.Player.controlLeft)
-							direction = CalamityMod.Enums.DashDirection.Left;
-						else
-							direction = CalamityMod.Enums.DashDirection.Right;
-						break;
-				}
-				result = true;
-				self.dashTimeMod = 0;
-			}
-			return result;
-		}
-		private delegate bool orig_HandleOmnidirectionalDash(CalamityMod.CalPlayer.CalamityPlayer self, out CalamityMod.Enums.DashDirection direction);
-		private static bool ODDashPatch(orig_HandleOmnidirectionalDash orig, CalamityMod.CalPlayer.CalamityPlayer self, out CalamityMod.Enums.DashDirection direction) {
-			direction = CalamityMod.Enums.DashDirection.Directionless;
-			bool result = false;
-			if (self.godSlayerDashHotKeyPressed) {
-				if (!(self.Player.controlLeft && self.Player.controlRight) && (self.Player.controlUp || self.Player.controlDown)) {
-					if (self.Player.controlUp)
-						direction = CalamityMod.Enums.DashDirection.Up;
-					if (self.Player.controlDown) {
-						direction = CalamityMod.Enums.DashDirection.Down;
-						self.Player.maxFallSpeed = 50f;
-					}
-				}
-				else switch (self.Player.direction) {
-					case -1:
-						if (self.Player.controlUp && self.Player.controlRight)
-							direction = CalamityMod.Enums.DashDirection.UpRight;
-						else if (self.Player.controlRight)
-							direction = CalamityMod.Enums.DashDirection.Right;
-						else if (self.Player.controlDown && self.Player.controlRight)
-							direction = CalamityMod.Enums.DashDirection.DownRight;
-						else
-							direction = CalamityMod.Enums.DashDirection.Left;
-						break;
-					case 1:
-						if (self.Player.controlUp && self.Player.controlLeft)
-							direction = CalamityMod.Enums.DashDirection.UpLeft;
-						else if (self.Player.controlLeft)
-							direction = CalamityMod.Enums.DashDirection.Left;
-						else if (self.Player.controlDown && self.Player.controlLeft) {
-							direction = CalamityMod.Enums.DashDirection.DownLeft;
-							self.Player.maxFallSpeed = 50f;
-						}
-						else
-							direction = CalamityMod.Enums.DashDirection.Right;
-						break;
-				}
-				result = true;
-				self.dashTimeMod = 0;
-			}
-			return result;
-		}
-		private static void ODDashCheckPatch(ILContext il) {
-			var c = new ILCursor(il);
-			if (!c.TryGotoNext(MoveType.Before, i => i.MatchLdfld(typeof(Player), "controlUp")))
-				throw new Exception("ControlUp field not found");
-			if (!c.TryGotoPrev(MoveType.Before, i => i.MatchLdarg(0)))
-				throw new Exception("Ldarg field not found");
-			c.RemoveRange(16);
+			Terraria.On_Player.DoCommonDashHandle -= VanillaDashPatch;
 		}
 	}
 }
